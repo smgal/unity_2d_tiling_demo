f2dc0089046618baa105f61e3a258b4374d746c2
Youngki Ahn <avej.com@gmail.com>
2017-10-09 22:15:31 +0900

    Initial commit
    Initial commit

---
+++ Assets/Source/Scripts/zMainLoop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zMainLoop : MonoBehaviour
+{
+	void Awake()
+	{
+		// DontDestroyOnLoad(...gameObject);
+	}
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	void OnApplicationQuit()
+	{
+		Debug.Log("Application ending after " + Time.time + " seconds");
+#if !UNITY_EDITOR
+		Application.CancelQuit();
+        System.Diagnostics.Process.GetCurrentProcess().Kill();
+#endif
+	}
+
+	public void OnMainMenuQuit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
